cf19de59c5768df7049553492d430184b123332e
GH-Gursimran <sohanpalgursimransingh@gmail.com>
2023-05-22 00:09:13 +0530

    Add project files.

---
+++ SampleCodePOC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCodePOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
